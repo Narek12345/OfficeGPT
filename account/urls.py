90c7b6da0db5cd8dc4a39d3c812f63a52b3eea30
Narek <narekbayanduryan16@gmail.com>
2023-12-14 00:48:57 +0300

    Configured password change.

--- account/urls.py
+++ account/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
 	# URL-адреса сброса пароля.
 	path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 	path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-	path('password-reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password-reset_confirm'),
+	path('password-reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 	path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
